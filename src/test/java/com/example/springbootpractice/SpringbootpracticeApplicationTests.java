53a484c4af2ea177b9d98d65eb523d9c47a4aa56
JeFeel <jaephil94@gmail.com>
2023-12-23 16:39:38 +0900

    initial setting

---
+++ src/test/java/com/example/springbootpractice/SpringbootpracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springbootpractice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringbootpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
