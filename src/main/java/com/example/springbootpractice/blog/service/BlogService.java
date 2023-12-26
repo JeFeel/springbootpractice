38de53dd41dfbff2fede0d61e5b82b1697509ef8
JeFeel <jaephil94@gmail.com>
2023-12-26 23:23:19 +0900

    add: BlogService 메소드 추가 게시글 모두 조회 게시글 하나 조회

--- src/main/java/com/example/springbootpractice/blog/service/BlogService.java
+++ src/main/java/com/example/springbootpractice/blog/service/BlogService.java
@@ -7,6 +7,9 @@ import com.example.springbootpractice.blog.domain.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -16,5 +19,15 @@ public class BlogService {
     // 게시글 추가
     public Article save(ArticleRequestDTO requestDTO){
         return blogRepository.save(requestDTO.toEntity());
+    }
+
+    // 게시글 목록 조회
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
+    // 게시글 하나 조회
+    public Article findById(long id){
+        return blogRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("article not found: "+id));
     }
 }
