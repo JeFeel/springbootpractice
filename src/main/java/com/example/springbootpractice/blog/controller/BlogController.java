623782e4bd5dbed7c83aeb80f3d430f38b1ba1b4
JeFeel <jaephil94@gmail.com>
2023-12-26 23:23:43 +0900

    add: BlogController 메소드 추가 게시글 모두 조회 게시글 하나 조회

--- src/main/java/com/example/springbootpractice/blog/controller/BlogController.java
+++ src/main/java/com/example/springbootpractice/blog/controller/BlogController.java
@@ -2,14 +2,15 @@ package com.example.springbootpractice.blog.controller;
 
 
 import com.example.springbootpractice.blog.domain.dto.ArticleRequestDTO;
+import com.example.springbootpractice.blog.domain.dto.ArticleResponseDTO;
 import com.example.springbootpractice.blog.domain.entity.Article;
 import com.example.springbootpractice.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -27,4 +28,21 @@ public class BlogController {
                 .body(blogService.save(requestDTO));
     }
 
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponseDTO>> findAllArticles(){
+        List<ArticleResponseDTO> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponseDTO::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponseDTO> findArticle(
+            @PathVariable long id
+    ){
+        Article article = blogService.findById(id);
+        return ResponseEntity.ok().body(new ArticleResponseDTO(article));
+    }
 }
