4745ade7c941b6e489a3a67c155ad96ca862ac7e
JeFeel <jaephil94@gmail.com>
2023-12-28 02:00:45 +0900

    delete unused import

--- src/test/java/com/example/springbootpractice/blog/controller/BlogControllerTest.java
+++ src/test/java/com/example/springbootpractice/blog/controller/BlogControllerTest.java
@@ -5,7 +5,6 @@ import com.example.springbootpractice.blog.domain.dto.ArticleRequestDTO;
 import com.example.springbootpractice.blog.domain.dto.UpdateArticleRequestDTO;
 import com.example.springbootpractice.blog.domain.entity.Article;
 import com.example.springbootpractice.blog.domain.repository.BlogRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
